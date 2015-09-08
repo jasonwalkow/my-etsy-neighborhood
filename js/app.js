ad4150366e919fb8c3f1a2b18834861f3dc7d134
Jason Walkow <jason.walkow1@gmail.com>
2015-09-07 19:13:31 -0700

    refactored js

--- js/app.js
+++ js/app.js
@@ -2,49 +2,26 @@ $(document).ready( function() {
 	$('.results').hide();
 	$('#map').hide();
 	$('.search__field__box').submit( function(event){
-		$('.map-placeholder').hide();
-		$('#map').show();
-        $('.results__boxes').html("");
-		$('.results').show();
 		getMap(findShops);
-		//showResults();
-		goToResults();
 	});
-
-	//Slow Scroll animated
-	var goToResults = function() {
-		$('html, body').animate({
-	         scrollTop: $("#results__title").offset().top
-	    		}, 1000);
-	}
-
+	
 // this function takes the shop object returned by Etsy 
 // and creates new result to be appended to DOM
 var showResults = function(shops) {
-
     // display default image if no icon is found
     var defaultImage = "../my-etsy-neighborhood/img/etsyicon.png";
-	
 	// clone our result template code
 	var result = $('.templates .results__boxes__shops').clone();
-	
 	// Set the profile image in result
 	result.find('.profile--image img').attr('src', shops.icon_url_fullxfull || defaultImage);
-
 	// Set shop name to show and link in result
-	var shopName = result.find('.shop-name a');
-	shopName.text(shops.shop_name);
-	shopName.attr('href', shops.url);
-
+    result.find('.shop-name a').text(shops.shop_name).attr('href', shops.url);
 	// Set tagline to show in result
 	result.find('.shop-tagline').text(shops.title);
-
     // Set shop admirers to show in result
     result.find('.admirers').text(shops.num_favorers);
-
     // Set shop listings number to show in result
     result.find('.listings').text(shops.listing_active_count);
-
 	return result;
 };
 
@@ -58,33 +35,33 @@ var findShops = function(options) {
         api_key: "id5s60fsw2x2ozw38lzpkq5h"
     };
 
-        var terms = $('#address').val();
-        var etsyURL = "https://openapi.etsy.com/v2/shops.js";
+    var terms = $('#address').val();
+    var etsyURL = "https://openapi.etsy.com/v2/shops.js";
 
-        $.ajax({
-            url: etsyURL,
-            data: requestData,
-            dataType: "JSONP",
-            jsonpCallback: "callback",
-            type: "GET",
-            success: function getData(data) {
-                if (data.ok) {
-                    if (data.count > 0) {
-                        $.each(data.results, function(i,item) {
-                            var sellers = showResults(item);
-                            $('.results__boxes').append(sellers);
-                        });
-                    } else {
-                        $('<p>No shops found.</p>').appendTo('.results__boxes');
-                    }
+    $.ajax({
+        url: etsyURL,
+        data: requestData,
+        dataType: "JSONP",
+        jsonpCallback: "callback",
+        type: "GET",
+        success: function getData(data) {
+            if (data.ok) {
+                if (data.count > 0) {
+                    $.each(data.results, function(i,item) {
+                        var sellers = showResults(item);
+                        $('.results__boxes').append(sellers);
+                    });
                 } else {
-                    $('.results__boxes').empty();
-                    alert(data.error);
+                    $('<p>No shops found.</p>').appendTo('.results__boxes');
                 }
+            } else {
+                $('.results__boxes').empty();
+                alert(data.error);
             }
-        });
+        }
+    });
 
-        return false;
+    return false;
 	}
 
 	var geocoder = new google.maps.Geocoder();
@@ -106,33 +83,39 @@ var findShops = function(options) {
     }
 
 	function getMap(findShops) {
+        $('.map-placeholder').hide();
+        $('#map').show();
+        $('.results__boxes').html("");
+        $('.results').show();
         //Get address from input field
         var address = document.getElementById("address").value;
         //Apply address to geocoder
-        geocoder.geocode( { address: address}, function(results, status) {
+        geocoder.geocode({address: address}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
-                findShops(results[0].geometry.location);
+                var location = results[0].geometry.location;
+                findShops(location);
                 var mapOptions = {
-                    center: results[0].geometry.location,
+                    center: location,
                     zoom: 13,
                     mapTypeControl: true,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 }
                 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                map.setCenter(results[0].geometry.location);
+                map.setCenter(location);
                 //Set address marker on map
                 var marker = new google.maps.Marker({
-                    position: results[0].geometry.location,
+                    position: location,
                     map: map,
                     title: "My Location"
                 });
-                var position = results[0].geometry.location;
-                var latlng = new google.maps.LatLng(position.G,position.K);
+                var latlng = new google.maps.LatLng(location.G,location.K);
                 geocoder.geocode({'latLng': latlng}, insertAddress);
             }
         });
-    }
-});
+        //Slow Scroll animated
+        $('html, body').animate({scrollTop: $("#results__title").offset().top}, 1000);
+        }
+    });
 
 
 
